2a4e7169f6c3d332789738b87260617d47dd8ad9
Sane4eck <77103844+Sane4eck@users.noreply.github.com>
2026-05-01 14:43:47 +0300

    Update __init__.py

--- controller/workers/__init__.py
+++ controller/workers/__init__.py
@@ -1,3 +1,4 @@
+from .hall_worker import hall_worker_main
 from .vesc_worker import vesc_worker_main
 from .psu_worker import psu_worker_main
 from .logger_worker import logger_worker_main
@@ -6,4 +7,5 @@ __all__ = [
     "vesc_worker_main",
     "psu_worker_main",
     "logger_worker_main",
+    "hall_worker_main",
 ]
